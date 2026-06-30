4b7801f4f0ada7b334d195e270d969a7b19b1574
AM5HOO <134503984+AM5HOO@users.noreply.github.com>
2026-07-01 00:00:11 +0530

    updated plugin

--- plugin/ollie.lua
+++ plugin/ollie.lua
@@ -1,7 +1,17 @@
-local M = {}
-
 function M.setup(opts)
     require("ollie.config").setup(opts)
-end
 
-return M
+    local g = vim.api.nvim_create_augroup('ollie', { clear = true })
+
+    vim.api.nvim_create_autocmd('VimEnter', {
+        group = g,
+        callback = function()
+            for _, v in ipairs(vim.v.argv) do
+                if v == '-' then
+                    vim.g.read_from_stdin = 1
+                    break
+                end
+            end
+        end,
+    })
+end
